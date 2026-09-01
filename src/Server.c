451baf04fc3f2f422585c2901d2fb09e285b30ad
Arad Segal <aradsegal12@gmail.com>
2026-09-01 23:55:45 +0300

    added a lot of new functionality, added an enture struct called http_response to store all of the relevent detailes for a respone fromt the server, add validate request, resolve_content_type and modified all the existing functions to work with the new structs and buisness logic. getting near to the end of the oroject

--- src/Server.c
+++ src/Server.c
@@ -15,6 +15,24 @@ typedef struct HttpHeader {
   char version[16];
 } http_header_t;
 
+typedef struct HttpResponse {
+  char resolved_path[256];
+  char content_type[64];
+  int status_code;
+
+} http_response_t;
+
+typedef struct {
+  const char *extension;
+  const char *content_type;
+} mime_type_t;
+
+mime_type_t mime_types[] = {{".html", "text/html"},
+                            {".css", "text/css"},
+                            {".js", "text/javascript"},
+                            {".png", "imrage/png"},
+                            {"jpeg", "image/jpeg"}};
+
 int send_all(int socket_fd, const void *buffer, size_t length) {
   const char *buf = buffer;
   size_t total_sent = 0;
@@ -33,7 +51,7 @@ int send_all(int socket_fd, const void *buffer, size_t length) {
   return 0;
 }
 
-int send_response(int socket_fd, char *file_path) {
+int send_response(http_response_t *http_response, int socket_fd) {
   char *error_response = "HTTP/1.1 404 Not Found\r\n"
                          "Content-Type: text/plain\r\n"
                          "Content-Length: 15\r\n"
@@ -41,13 +59,11 @@ int send_response(int socket_fd, char *file_path) {
                          "\r\n"
                          "404 Not found\r\n";
 
-  FILE *html_file = fopen(file_path, "rb");
-  if (html_file == NULL) {
-    printf("file_path: %s\n", file_path);
-    send_all(socket_fd, error_response, strlen(error_response));
-    perror("fopen");
-    return -1;
+  if (http_response->status_code != 200) {
+    // return error page with corresponding code
   }
+
+  FILE *html_file = fopen(http_response->resolved_path, "rb");
   if (fseek(html_file, 0, SEEK_END) == -1) {
     perror("fseek");
     fclose(html_file);
@@ -62,12 +78,13 @@ int send_response(int socket_fd, char *file_path) {
   rewind(html_file);
   char http_header[512];
   int header_length = snprintf(http_header, sizeof(http_header),
-                               "HTTP/1.1 200 OK\r\n"
-                               "Content-Type: text/html\r\n"
+                               "HTTP/1.1 %d OK\r\n"
+                               "Content-Type: %s\r\n"
                                "Content-Length: %ld\r\n"
                                "Connection: close\r\n"
                                "\r\n",
-                               file_size);
+                               http_response->status_code,
+                               http_response->content_type, file_size);
 
   if (header_length < 0 || (size_t)header_length >= sizeof(http_header)) {
     fprintf(stderr, "failed to build the HTTP header\n");
@@ -122,27 +139,64 @@ int parse_request(http_header_t *http_header, char *received_buf) {
   return 0;
 }
 
-int resolve_request(char *file_path, int file_path_size,
-                    http_header_t http_header) {
+int resolve_path(http_response_t *http_response, int file_path_size,
+                 http_header_t http_header) {
   int bytes_read;
   if (strcmp(http_header.path, "/") == 0) {
-    bytes_read = snprintf(file_path, file_path_size, "public/index.html");
+    bytes_read = snprintf(http_response->resolved_path, file_path_size,
+                          "public/index.html");
   } else {
-    bytes_read =
-        snprintf(file_path, file_path_size, "public%s", http_header.path);
+    bytes_read = snprintf(http_response->resolved_path, file_path_size,
+                          "public%s", http_header.path);
   }
   if (bytes_read < 0) {
     fprintf(stderr, "error while resolving the request");
     return -1;
   } else if (bytes_read >= file_path_size) {
-    fprintf(stderr, "the path variable is to small\n");
+    fprintf(stderr, "the path variable is too small\n");
     return bytes_read;
   }
   return 0;
 }
+
+int resolve_content_type(http_response_t *http_response) {
+  char *extension = strrchr(http_response->resolved_path, '.');
+  if (extension == NULL) {
+    fprintf(stderr, "no file extension in path");
+    return -1;
+  }
+  for (int i = 0; i < sizeof(mime_types) / sizeof(mime_type_t); i++) {
+    if (strcmp(extension, mime_types[i].extension) == 0) {
+      strcpy(http_response->content_type, mime_types[i].content_type);
+      return 0;
+    }
+  }
+  strcpy(http_response->content_type, "application/octet-stream");
+  return -2;
+}
+
+int validate_request(http_response_t *http_response,
+                     http_header_t http_header) {
+  if (strcmp(http_header.method, "GET") != 0) {
+    fprintf(stderr, "the method: {%s} is not allowd", http_header.method);
+    return 405;
+  }
+  if (access(http_response->resolved_path, F_OK) != 0) {
+    fprintf(stderr, "the path: {%s} doesnt exist", http_header.path);
+    return 404;
+  }
+  if (strcmp(http_header.version, "HTTP/1.1")) {
+    fprintf(stderr, "the version: {%s}, is not allowd on this website",
+            http_header.version);
+    return 400;
+  }
+  return 200;
+}
+
 int handle_request(int socket_fd) {
   char received_buf[2048];
   http_header_t http_header;
+  http_response_t http_response;
   char file_path[256];
   memset(received_buf, 0, sizeof(received_buf));
   ssize_t bytes_read = 0;
@@ -163,8 +217,8 @@ int handle_request(int socket_fd) {
     received_buf[bytes_read] = '\0';
   }
 
-  printf("ended loop\n bytes_read: %d, sizeof(received_buf): %d\n", bytes_read,
-         sizeof(received_buf));
+  printf("ended loop\n bytes_read: %lu, sizeof(received_buf): %lu\n",
+         bytes_read, sizeof(received_buf));
   if (strstr(received_buf, "\r\n\r\n") != NULL) {
     printf("were good\n");
     printf("---- NEW MESSAGE RECEIVED ----\n%s\n-----------------------\n",
@@ -174,11 +228,16 @@ int handle_request(int socket_fd) {
       fprintf(stderr, "error ecourd in function parse_request");
       return -1;
     }
-    if (resolve_request(file_path, 256, http_header) == -1) {
+    if (resolve_path(&http_response, sizeof(http_response.resolved_path),
+                     http_header) == -1) {
       fprintf(stderr, "error ecourd in function resolve_request");
+      return -1;
     }
 
-    if (send_response(socket_fd, file_path) == -1) {
+    validate_request(&http_response, http_header);
+    resolve_content_type(&http_response);
+
+    if (send_response(&http_response, socket_fd) == -1) {
       close(socket_fd);
       return -1;
     }
